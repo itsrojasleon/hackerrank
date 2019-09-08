6a8c0346b95c53062a854740bf921951443ba585
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-09-07 19:29:57 -0500

    simple-array-sum.js

---
+++ algorithms/simple-array-sum.js
@@ -0,0 +1,48 @@
+/*
+  Simple Array Sum
+*/
+
+/*
+  https://www.hackerrank.com/challenges/simple-array-sum/problem
+*/
+
+/*
+  Given an array of integers, find the sum of its elements.
+
+  For example, if the array ar = [1,2,3], 1 + 2 + 3 = 6 , so return 6.
+
+  Function Description
+
+  Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+
+  simpleArraySum has the following parameter(s):
+
+  ar: an array of integers
+  Input Format
+
+  The first line contains an integer, , denoting the size of the array.
+  The second line contains  space-separated integers representing the array's elements.
+
+  Constraints
+  0 < n, ar[i] <= 1000
+
+  Output Format
+
+  Print the sum of the array's elements as a single integer.
+
+  Sample Input
+
+  6
+  1 2 3 4 10 11
+  Sample Output
+
+  31
+  Explanation
+
+  We print the sum of the array's elements: 1+2+3+4+10+11 = 31.
+*/
+
+function simpleArraySum(ar) {
+  return ar.reduce((a, b) => a + b, 0);
+}
+simpleArraySum([1, 2, 3]);
