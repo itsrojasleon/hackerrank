fc5040e9018484476b4a26f6ea70b4454ebaca57
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-09-08 01:36:18 -0500

    js10-hello-world.js

---
+++ 10-days-of-javascript/js10-hello-world.js
@@ -0,0 +1,13 @@
+/*
+  Day 0: Hello World!  
+*/
+/*
+  https://www.hackerrank.com/challenges/js10-hello-world/problem
+*/
+
+function greeting(parameterVariable) {
+  console.log('Hello, World!');
+  console.log(parameterVariable);
+}
+greeting("Welcome to 10 Days of JavaScript!");
+
