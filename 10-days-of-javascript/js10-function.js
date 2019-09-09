18be4a38d60087e6efb34cb458d486d1d4e1b1ee
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-09-08 20:21:24 -0500

    js10-function.js

---
+++ 10-days-of-javascript/js10-function.js
@@ -0,0 +1,15 @@
+/*
+  Day 1: Functions
+*/
+/*
+  https://www.hackerrank.com/challenges/js10-function/problem
+*/
+
+function factorial(n) {
+  let result;
+
+  for (let i = n; i > 0; i--) {
+    result *= i;
+  }
+  return result;
+}
