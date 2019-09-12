279f52f82a0a668e1597a93b326d497c1b2ea28d
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-09-12 12:36:17 -0500

    python-division.py

---
+++ python/python-division.py
@@ -0,0 +1,8 @@
+# Python: Division
+# https://www.hackerrank.com/challenges/python-division/problem
+
+if __name__ == '__main__':
+  a = int(input())
+  b = int(input())
+  print(int(a / b))
+  print(float(a / b))
