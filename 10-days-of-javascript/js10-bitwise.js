23a90aca01a6c18de7b150d4dcb3edea9ba0c803
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-09-14 14:54:23 -0500

    js10-bitwise.js

---
+++ 10-days-of-javascript/js10-bitwise.js
@@ -0,0 +1,35 @@
+/*
+  Day 6: Bitwise Operators
+*/
+/*
+  https://www.hackerrank.com/challenges/js10-bitwise/problem
+*/
+
+'use strict';
+
+function getMaxLessThanK(n, k) {
+  let bitAnd = 0;
+  let max = 0;
+  let j;
+  for (let i = 1; i <= n; i++) {
+    for (j = i + 1; j <= n; j++) {
+      bitAnd = i & j;
+      if (bitAnd > max && bitAnd < k) {
+        max = bitAnd;
+      }
+    }
+  }
+  return max;
+}
+
+function main() {
+  const q = +readLine();
+
+  for (let i = 0; i < q; i++) {
+    const [n, k] = readLine()
+      .split(' ')
+      .map(Number);
+
+    console.log(getMaxLessThanK(n, k));
+  }
+}
