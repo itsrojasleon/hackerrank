ba20f10d2ed64f9910c80a6375dbbfda38e2eb81
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-11-24 14:23:36 -0600

    drawing-book

---
+++ algorithms/implementation/drawing-book.js
@@ -0,0 +1,23 @@
+/*
+
+*/
+/*
+  https://www.hackerrank.com/challenges/drawing-book/problem
+*/
+
+function pageCount(n, p) {
+  let a = 0;
+  let b = 0;
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 === 0) a++;
+    if (i === p) break;
+  }
+
+  for (let i = n; i >= 1; i--) {
+    if (i === p) break;
+    if (i % 2 === 0) b++;
+  }
+
+  return a < b ? a : b;
+}
